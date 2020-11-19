dff5d7157b9a62aa605745bca802dc59c68bcba3
William Truppi <williamt@hotmail.it>
2020-11-19 15:22:16 +0100

    Esercizio completato con stampa su html e console
    segue un migliore adeguamento estetico del file html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 // creazione array
 var arrayNum = [];
 var firstArray = document.getElementById("first_array");
-/* var lastArray = document.getElementById(first_array); */
+var lastArray = document.getElementById("last_array");
 
+//popolamento array
 for( var i=0; i < 100; i++){
   arrayNum.push(i+1);
-  firstArray.innerHTML = arrayNum;
+}
+console.log(arrayNum);
+firstArray.innerHTML = "Array iniziale: " + arrayNum;
+
+//controllo multipli
+for( var i=0; i < 100; i++){
+  if (((arrayNum[i] % 3) == 0) && ((arrayNum[i] % 5) == 0)){
+    arrayNum[i] = "FizzBuzz";
+  } else if ((arrayNum[i] % 5) == 0){
+    arrayNum[i] = "Buzz";
+  } else if ((arrayNum[i] % 3) == 0){
+    arrayNum[i] = "Fizz";
+  }
 }
 
+//STAMPA RISULTATO SU HTML
 console.log(arrayNum);
+lastArray.innerHTML = "Array MODIFICATO: " +  arrayNum;
