2f321b59c078ecd8199176a1be5a3ace22975c69
William Truppi <williamt@hotmail.it>
2020-11-19 17:09:12 +0100

    output su html resa più leggibile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,30 @@
 // creazione array
 var arrayNum = [];
-var firstArray = document.getElementById("first_array");
-var lastArray = document.getElementById("last_array");
 
 //popolamento array
 for( var i=0; i < 100; i++){
   arrayNum.push(i+1);
+  var firstArrayItem = arrayNum[i];
+  var firstArray = document.getElementById("first_array").innerHTML;
+  document.getElementById("first_array").innerHTML = firstArray + "<li>" + firstArrayItem + "</li>";
 }
+
 console.log(arrayNum);
-firstArray.innerHTML = "Array iniziale: " + arrayNum;
 
 //controllo multipli
 for( var i=0; i < 100; i++){
   if (((arrayNum[i] % 3) == 0) && ((arrayNum[i] % 5) == 0)){
-    arrayNum[i] = "FizzBuzz";
+    arrayNum[i] = "<strong>" + "FizzBuzz" + "</strong>";
   } else if ((arrayNum[i] % 5) == 0){
     arrayNum[i] = "Buzz";
   } else if ((arrayNum[i] % 3) == 0){
     arrayNum[i] = "Fizz";
   }
+
+  var lastArrayItem = arrayNum[i];
+  var lastArray = document.getElementById("last_array").innerHTML;
+  document.getElementById("last_array").innerHTML = lastArray + "<li> -----> " + lastArrayItem + "</li>";
 }
 
 //STAMPA RISULTATO SU HTML
 console.log(arrayNum);
-lastArray.innerHTML = "Array MODIFICATO: " +  arrayNum;
